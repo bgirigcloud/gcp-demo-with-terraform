9c748ef22f1bec69d8d1f589181f6895e44a85f1
tush-tr <tusharrajput604@gmail.com>
2023-02-13 18:23:31 +0530

    fix(PLAN): Remove destroy

--- main.tf
+++ main.tf
@@ -2,10 +2,13 @@ module "VPC" {
   source = "./tf-modules/vpc" 
   region = var.region
 }
-# module "vm-instance" {
-#   source = "./tf-modules/vm-instance"
-# }
-# module "disk" {
-#   source = "./tf-modules/attached-multiple-disk-with-vm"
-#   project_id = var.project_id
-# }
+module "vm-instance" {
+  source = "./tf-modules/vm-instance"
+  region = var.region
+  project_id = var.project_id
+  
+}
+module "disk" {
+  source = "./tf-modules/attached-multiple-disk-with-vm"
+  project_id = var.project_id
+}
