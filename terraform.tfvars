cb4fbb71ee976ad8b31b58f6e48d95be5384f317
Biswanath Giri <biswanathgiri@192.168.1.16>
2023-02-14 18:18:15 +0530

    vpc mudule changed

--- terraform.tfvars
+++ terraform.tfvars
@@ -3,7 +3,7 @@ network           = "custom-vpc-tf"
 subnetwork        = "10.1.0.0/24"
 region            = "us-central1"
 zone              = "us-central1-a"
-health_check      = "lb_health_check"
+
 instance_template = "ist_instance_template"
 service_account   = "testsraccountsformigwise-ally-348408"
 target_pools = "test_public_target-pool" 
--- tf-modules/vpc/variables.tf
+++ tf-modules/vpc/variables.tf
@@ -3,25 +3,7 @@
 } 
 */
 
-variable "region" { 
 
-} 
 
- variable "instance_subnetwork" {
- 
-} 
-variable "network" {
- 
-}
- variable "instance_template" {
-  description = "Instance template self_link used to create compute instances"
-  default = "test"
-} 
 
-variable "service_account" {
-  default = "tcpl"
-}
-variable "health_check" {
-  
-}
  
